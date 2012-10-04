6ae23d00dd46852ff3147fd913611a55f09246ba
eugenemark <eugenemark@820d3149-562b-4f88-9aa4-a8e61a3485cf>
2012-10-04 09:44:32 +0000

    Tutorial. Chapter3. C3_01_...test added.
    git-svn-id: svn://svn.code.sf.net/p/itextsharp/code@440 820d3149-562b-4f88-9aa4-a8e61a3485cf

---
+++ tutorial/signatures/chapter3/C3_01_SignWithCAcert/C3_01_SignWithCAcert.cs
@@ -0,0 +1,72 @@
+/*
+ * This class is part of the white paper entitled
+ * "Digital Signatures for PDF documents"
+ * written by Bruno Lowagie
+ * 
+ * For more info, go to: http://itextpdf.com/sales
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.util;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.X509;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.security;
+
+namespace signatures.chapter3 {
+    public class C3_01_SignWithCAcert {
+        public static String SRC = "../../../../resources/hello.pdf";
+        public static String DEST = "../../../../results/chapter3/hello_cacert.pdf";
+
+        public void Sign(String src, String dest,
+                         ICollection<X509Certificate> chain, ICipherParameters pk,
+                         String digestAlgorithm, CryptoStandard subfilter,
+                         String reason, String location,
+                         ICollection<ICrlClient> crlList,
+                         IOcspClient ocspClient,
+                         ITSAClient tsaClient,
+                         int estimatedSize) {
+            // Creating the reader and the stamper
+            PdfReader reader = new PdfReader(src);
+            FileStream os = new FileStream(dest, FileMode.OpenOrCreate);
+            PdfStamper stamper = PdfStamper.CreateSignature(reader, os, '\0');
+            // Creating the appearance
+            PdfSignatureAppearance appearance = stamper.SignatureAppearance;
+            appearance.Reason = reason;
+            appearance.Location = location;
+            appearance.SetVisibleSignature(new Rectangle(36, 748, 144, 780), 1, "sig");
+            // Creating the signature
+            IExternalSignature pks = new PrivateKeySignature(pk, digestAlgorithm);
+            MakeSignature.SignDetached(appearance, pks, chain, crlList, ocspClient, tsaClient, estimatedSize, subfilter);
+        }
+
+        public static void Main(String[] args) {
+            Properties properties = new Properties();
+            properties.Load(new FileStream("c:/home/blowagie/key.properties", FileMode.Open));
+            String path = properties["PRIVATE"];
+            char[] pass = properties["PASSWORD"].ToCharArray();
+
+            Pkcs12Store ks = new Pkcs12Store();
+            ks.Load(new FileStream(path, FileMode.Open), pass);
+            String alias = "";
+            foreach (string al in ks.Aliases) {
+                if (ks.IsKeyEntry(al) && ks.GetKey(al).Key.IsPrivate) {
+                    alias = al;
+                    break;
+                }
+            }
+            AsymmetricKeyEntry pk = ks.GetKey(alias);
+            ICollection<X509Certificate> chain = new List<X509Certificate>();
+            foreach (X509CertificateEntry entry in ks.GetCertificateChain(alias)) {
+                chain.Add(entry.Certificate);    
+            }
+            C3_01_SignWithCAcert app = new C3_01_SignWithCAcert();
+            app.Sign(SRC, DEST, chain, pk.Key, DigestAlgorithms.SHA256, CryptoStandard.CMS, "Test",
+                     "Ghent", null, null, null, 0);
+        }
+    }
+}
