7f18c1c9cff68ac084c18617ac4425e1cd2aaa94
eugenemark <eugenemark@820d3149-562b-4f88-9aa4-a8e61a3485cf>
2013-04-09 14:49:29 +0000

    5.4.1
    git-svn-id: svn://svn.code.sf.net/p/itextsharp/code@540 820d3149-562b-4f88-9aa4-a8e61a3485cf

---
+++ branches/iTextSharp_5_4_1/src/core/System/util/collections/HashSet2.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace System.util.collections
+{
+    public class HashSet2<T>: ICollection<T>
+    {
+        private Dictionary<T, object> set;
+
+        public HashSet2()
+        {
+            set = new Dictionary<T, object>();
+        }
+
+        public HashSet2(IEnumerable<T> set) : this()
+        {
+            foreach (T item in set)
+                Add(item);
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            return set.Keys.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            set[item] = null;
+        }
+
+        public void Clear()
+        {
+            set.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return set.ContainsKey(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            set.Keys.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return set.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return set.Count; }
+        }
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+    }
+}
